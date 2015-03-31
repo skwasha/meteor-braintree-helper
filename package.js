e8a02da0149357eb62e0bca921e2e7f1c2693e4e
Sascha Linn <sascha@umich.edu>
2015-03-30 22:47:54 -0700

    update package

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'hiukim:braintree-helper',
+  name: 'skwasha:braintree-helper',
   version: '1.0.0',
   // Brief, one-line summary of the package.
   summary: 'Just a braintree helper',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/hiukim/meteor-braintree-helper.git',
+  git: 'https://github.com/skwasha/meteor-braintree-helper.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -13,15 +13,15 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.0.3.2');
   api.addFiles('braintree-client-v2.js', 'client');
-  api.addFiles('hiukim:braintree-helper.js', 'server');
+  api.addFiles('skwasha:braintree-helper.js', 'server');
   api.export('BraintreeHelper','server');
   api.export('Braintree','server');
 });
 
 Package.onTest(function(api) {
   api.use('tinytest');
-  api.use('hiukim:braintree-helper');
-  api.addFiles('hiukim:braintree-tests.js');
+  api.use('skwasha:braintree-helper');
+  api.addFiles('skwasha:braintree-tests.js');
 });
 
 Npm.depends({
